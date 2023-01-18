bb25e8caa4226bb1c2852e01d4f956ef047625c3
Tazo Gorgadze <99098290+NeRo707@users.noreply.github.com>
2023-01-18 18:25:39 +0400

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,15 +2,6 @@ import  firebase  from 'firebase';
 
 
 
-const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyBT8AmXa4N2G04UROpASaJSpxdNdJZaXE4",
-  authDomain: "chatapp-a7506.firebaseapp.com",
-  projectId: "chatapp-a7506",
-  storageBucket: "chatapp-a7506.appspot.com",
-  messagingSenderId: "707682467027",
-  appId: "1:707682467027:web:f2f2eb44f5d22216452826",
-  measurementId: "G-0QD5QYF89S"
-});
 
 
 
